53946f13a7c10ba4c3c32941d183d09d4543ec07
Tyler Norbury <tylernorbury96@gmail.com>
2020-03-14 19:02:54 -0700

    Moved all the sections of the page into their own folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 import Header from "../components/header/header"
-import Intro from "../components/intro/intro"
-import About from "../components/about/about"
-import Projects from "../components/projects/projects"
+import Intro from "../components/sections/intro/intro"
+import About from "../components/sections/about/about"
+import Projects from "../components/sections/projects/projects"
 
 const IndexPage = () => (
   <Layout>
